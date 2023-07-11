bc9d25adafb62d6540456d68507a41954554d446
yeeuniii <anna862700@gmail.com>
2023-07-11 16:45:49 +0900

    Study: Point & Geometry 클래스 생성

---
+++ point/point.cpp
@@ -0,0 +1,7 @@
+#include "point.hpp"
+
+Point::Point(int pos_x, int pos_y)
+{
+	this->x = pos_x;
+	this->y = pos_y;
+}
---
+++ point/point.hpp
@@ -0,0 +1,13 @@
+#ifndef POINT_H
+# define POINT_H
+
+class	Point
+{
+	int	x;
+	int	y;
+
+	public:
+	 Point(int pos_x, int pos_y);
+}
+
+#endif
