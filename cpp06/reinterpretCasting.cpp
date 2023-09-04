e011534e2ed1f9aeecd4faf1b250abb5476ca92d
yeeuniii <anna862700@gmail.com>
2023-09-04 18:52:01 +0900

    Study: 포인터와 scalar 자료형 간 reinterpret cast

--- cpp06/reinterpretCasting.cpp
+++ cpp06/reinterpretCasting.cpp
@@ -4,11 +4,12 @@ int	main()
 {
 	int	*intPtr = new int(3);
 	
-	int _int = reinterpret_cast<int>(intPtr);
-	std::cout << _int << std::endl;
+//	int _int = reinterpret_cast<int>(intPtr);
+//	std::cout << _int << std::endl;
 
 	long _long = reinterpret_cast<long>(intPtr);
 	std::cout << _long << std::endl;
 
+	int	*ptr = reinterpret_cast<int*>(_long);
 	return 0;
 }
